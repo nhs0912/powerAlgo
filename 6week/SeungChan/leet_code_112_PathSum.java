7011f50bd815bf3fca6ff6d69e175055e5a91941
Seung-chan, Baeg <mokdonjr@naver.com>
2017-08-03 16:18:11 +0900

    Create leet_code_112_PathSum.java

---
+++ 6week/SeungChan/leet_code_112_PathSum.java
@@ -0,0 +1,16 @@
+public class Solution {
+    
+    public boolean hasPathSum(TreeNode root, int sum) {
+        
+        if(root == null) return false;
+        
+        sum -= root.val;
+        
+        if(root.left == null && root.right == null){
+            if(sum == 0) return true;
+            else return false;
+        }
+        else 
+            return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+}

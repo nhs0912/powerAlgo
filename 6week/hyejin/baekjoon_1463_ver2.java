94e7fffaa55289f031af3c43d9b76e036784f39d
Hyejin Lee <hyejin.lee0805@gmail.com>
2017-08-06 21:47:52 +0900

    성공

--- 6week/hyejin/baekjoon_1463_ver2.java
+++ 6week/hyejin/baekjoon_1463_ver2.java
@@ -11,11 +11,12 @@ public class Main {
 		dp[0] = dp[1] = 0;
 		
 		for(int i=2; i<=X; i++){			
-			dp[i] = dp[i-1] + 1;
 			if(i % 3 == 0)
 				dp[i] = ( dp[i/3] < dp[i-1] ? dp[i/3] : dp[i-1] ) + 1; 
-			if(i % 2 == 0)
+			else if(i % 2 == 0)
 				dp[i] = ( dp[i/2] < dp[i-1] ? dp[i/2] : dp[i-1] ) + 1;
+			else
+				dp[i] = dp[i-1] + 1;
 		}
 		
 		System.out.println(dp[X]);

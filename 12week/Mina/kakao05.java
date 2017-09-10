aedcf22e097bc0f8986028b9833bd6561d766292
Kim Min A <premiummina@gmail.com>
2017-09-10 17:28:54 +0900

    Create kakao05.java

---
+++ 12week/Mina/kakao05.java
@@ -0,0 +1,22 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        int r =  land.length;
+        int[][] dp = new int[r][4];
+        
+        dp[0][0] = land[0][0];
+		dp[0][1] = land[0][1];
+		dp[0][2] = land[0][2];
+		dp[0][3] = land[0][3];
+
+        for (int i = 1; i < r; i++) {
+			dp[i][0] = land[i][0] + Math.max(Math.max(dp[i-1][1], dp[i-1][2]), dp[i-1][3]);
+			dp[i][1] = land[i][1] + Math.max(Math.max(dp[i-1][0], dp[i-1][2]), dp[i-1][3]);
+			dp[i][2] = land[i][2] + Math.max(Math.max(dp[i-1][0], dp[i-1][1]), dp[i-1][3]);
+			dp[i][3] = land[i][3] + Math.max(Math.max(dp[i-1][0], dp[i-1][1]), dp[i-1][2]);
+		}
+        
+        answer = Math.max(Math.max(dp[r - 1][0], dp[r - 1][1]), Math.max(dp[r - 1][2], dp[r - 1][3]));
+        return answer;
+    }
+}
